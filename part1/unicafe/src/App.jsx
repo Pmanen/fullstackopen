2ecb2fc0c31cc2d0182a9c837923b6afbe0d543d
Pippa van Manen <pippavanmanen@icloud.com>
2025-11-19 23:44:55 +0100

    ex 1.7 unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,22 +12,50 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [score, setScore] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [positive, setPositive] = useState(0.0)
 
-  const handleFeedback = (val, setVal) => () => setVal(val + 1)
+  const handleFeedback = (val, setVal, type) => () => {
+    const newVal = val + 1
+    const newAll = all + 1
+    let newScore = score
+    let newPositive = positive * all / 100
+    setVal(newVal)
+    setAll(newAll)
+    if (type === "good") {
+      newScore = score + 1
+      newPositive = (newPositive + 1) / newAll * 100
+    }
+    else if (type === "bad") {
+      newScore = score - 1
+      newPositive = newPositive / newAll * 100
+    }
+    else if (type === "neutral") {
+      newPositive = newPositive / newAll * 100
+    }
+    setAvg(newScore / newAll)
+    setScore(newScore)
+    setPositive(newPositive)
+  }
 
   return (
     <div>
       <Header text="give feedback" />
       <div>
-        <Button onClick={handleFeedback(good, setGood)} text="good" />
-        <Button onClick={handleFeedback(neutral, setNeutral)} text="neutral" />
-        <Button onClick={handleFeedback(bad, setBad)} text="bad" />
+        <Button onClick={handleFeedback(good, setGood, "good")} text="good" />
+        <Button onClick={handleFeedback(neutral, setNeutral, "neutral")} text="neutral" />
+        <Button onClick={handleFeedback(bad, setBad, "bad")} text="bad" />
       </div>
       <Header text="statistics" />
       <div>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {avg}</p>
+        <p>positive {positive} %</p>
       </div>
     </div>
   )
