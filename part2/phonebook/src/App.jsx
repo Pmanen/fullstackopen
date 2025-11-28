4f7208822db0c754261962ad21b0505b331302a7
Pippa van Manen <pippavanmanen@icloud.com>
2025-11-28 18:46:52 +0100

    ex 2.7 Phonebook step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -9,8 +9,14 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = { name: newName }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
+ 
   }
 
   const handleFormChange = (event) => {
