e1182f96aeeee10d7de39d3c0c6c5cc51d1fffb3
Pippa van Manen <pippavanmanen@icloud.com>
2025-11-30 14:10:11 +0100

    ex 2.13 Phonebook step 8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -11,8 +11,8 @@ const App = () => {
   const [filterIsEmpty, setFilterIsEmpty] = useState(true)
 
   useEffect(() => {
-      axios.get('http://localhost:3001/persons').then((response) => {
-      setPersons(response.data)
+      personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons)
     })
   }, []) 
 
@@ -23,16 +23,12 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     }
     else {
-      axios
-        .post('http://localhost:3001/persons', newPerson)
-        .then((response) => {
-          setPersons(persons.concat(response.data))
-          setNewName('')
-          setNewNumber('')
-        })
-   
+      personService.create(newPerson).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
     }
- 
   }
 
   const handleNameChange = (event) => {
