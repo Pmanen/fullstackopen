f1678f9adcbc73157faf282bd131a6920b216f95
Pippa van Manen <pippavanmanen@icloud.com>
2025-12-08 20:55:02 +0100

    ex 4.2 blog list step 2 - refactor models and controllers

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,36 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
 app.use(express.json())
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
+mongoose.connect(config.MONGODB_URI, { family: 4 })
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { family: 4 })
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  console.log(`blog: ${blog}`)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
