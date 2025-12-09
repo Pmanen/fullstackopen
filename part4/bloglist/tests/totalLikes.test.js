bf1cbc7202ea51fb77cd1f367c4d7a14598f6684
Pippa van Manen <pippavanmanen@icloud.com>
2025-12-09 15:45:07 +0100

    ex 4.4 helper functions and unit tests step 2

---
+++ part4/bloglist/tests/totalLikes.test.js
@@ -0,0 +1,52 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+describe('total likes', () => {
+  listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  listWithNoBlog = []
+
+  listWithTwoBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 1,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 999,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has no blog, equals 0', () => {
+    const result = listHelper.totalLikes(listWithNoBlog)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has two blog, equals sum of likes', () => {
+    const result = listHelper.totalLikes(listWithTwoBlog)
+    assert.strictEqual(result, 1000)
+  })
+})
