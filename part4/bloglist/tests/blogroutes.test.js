f90cf4852e50e8e76d70bc17ee858c12a9e9aaf5
Pippa van Manen <pippavanmanen@icloud.com>
2025-12-10 17:50:55 +0100

    ex 4.10 blog list tests step 3

--- part4/bloglist/tests/blogroutes.test.js
+++ part4/bloglist/tests/blogroutes.test.js
@@ -1,11 +1,11 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const assert = require('node:assert')
 const Blog = require('../models/blog')
-const { initialBlogs } = require('./test_helper')
+const { initialBlogs, blogsInDb } = require('./test_helper')
 
 const api = supertest(app)
 
@@ -14,7 +14,7 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs) 
 })
 
-test('get returns correct # blogs in json format', async () => {
+test.only('get returns correct # blogs in json format', async () => {
   const response = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
 
   assert.strictEqual(response.body.length, initialBlogs.length)
@@ -24,6 +24,27 @@ test.only('returned id property is named id and not _id', async () => {
   const response = await api.get('/api/blogs')
 
   assert(response.body[0].id && !(response.body[0]._id))
+})
+
+describe('post', () => {
+  test.only('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Exercise is good',
+      author: 'Allison Jerk',
+      url: 'https://home.page',
+      likes: 0
+    }
+
+   const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const newDatabase = await blogsInDb()
+    assert.strictEqual(newDatabase.length, initialBlogs.length + 1)
+    assert.strictEqual(response.body.title, newBlog.title)
+  })
 })
 
 after(async () => {
