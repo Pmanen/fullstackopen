8c38e3e143190ea2a293da8787b1b8d69c826f61
Pippa van Manen <pippavanmanen@icloud.com>
2025-12-12 20:47:54 +0100

    ex 4.21 blog list expansion step 8

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -12,6 +12,7 @@ app.use(express.json())
 
 mongoose.connect(config.MONGODB_URI, { family: 4 })
 
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
