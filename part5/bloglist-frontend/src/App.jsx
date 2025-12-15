d0febf3d8df143884e10c9e02a0b0143d51a7205
Pippa van Manen <pippavanmanen@icloud.com>
2025-12-15 20:55:21 +0100

    ex 5.3 blog list frontend step 3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -41,6 +44,27 @@ const App = () => {
     }
   }
 
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    const newBlog = {
+      title: title,
+      author: author,
+      url: url,
+      likes: 0,
+    }
+    try {
+      const response = await blogService.create(newBlog)
+      setBlogs(blogs.concat(response))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch {
+      console.log('invalid blog')
+    }
+
+    console.log('create')
+  }
+
   const logout = (event) => {
     window.localStorage.removeItem('loggedBlogappUser')
     blogService.setToken(null)
@@ -65,6 +89,33 @@ const App = () => {
     </form>
   )
 
+  const createForm = () => (
+    <div>
+      <h2>Create New</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          <label>
+            title: 
+            <input  type="text" value={title} onChange={({ target }) => setTitle(target.value)}/>
+          </label>
+        </div>
+        <div>
+          <label>
+            author: 
+            <input  type="text" value={author} onChange={({ target }) => setAuthor(target.value)}/>
+          </label>
+        </div>
+        <div>
+          <label>
+            url: 
+            <input  type="text" value={url} onChange={({ target }) => setUrl(target.value)}/>
+          </label>
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+
   const blogList = () => (
     <div>
       <h2>blogs</h2>
@@ -77,6 +128,7 @@ const App = () => {
   return (
     <div>
       {user ? <p>{user.name} is logged in <button onClick={() => logout()}>logout</button></p> : loginForm()}
+      {user && createForm()}
       {user && blogList()}
     </div>
   )
