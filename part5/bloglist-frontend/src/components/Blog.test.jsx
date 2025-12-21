34d26f777276b76825e3797496eec3249603e94a
Pippa van Manen <pippavanmanen@icloud.com>
2025-12-21 21:29:07 +0100

    ex 5.15 blog list tests step 3

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import blogService from '../services/blogs'
 
 test('render title and author, don\'t render likes and url at start', () => {
   const blog = {
@@ -44,4 +45,27 @@ test('after clicking the button, url and likes are shown', async () => {
   const urlElement = screen.getByText('https://example.com')
   expect(likesElement).toBeVisible()
   expect(urlElement).toBeVisible()
+})
+
+test('after clicking like twice, like function is called twice and likes are increased by 2', async () => {
+  const blog = {
+    title: 'Test Title IV',
+    author: 'Testius',
+    likes: 5,
+    user: 89139721837,
+    url: 'https://example.com'
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const likeButton = screen.getByText('like')
+  const updateSpy = vi.spyOn(blogService, 'update').mockResolvedValue({})
+
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  const likesElement = screen.getByText('likes: 7', { exact: false })
+  expect(updateSpy).toHaveBeenCalledTimes(2)
+  expect(likesElement).toBeDefined()
 })
