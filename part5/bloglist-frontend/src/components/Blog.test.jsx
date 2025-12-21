032f706452955ce18a721027ef08598ed96d76eb
Pippa van Manen <pippavanmanen@icloud.com>
2025-12-21 20:49:44 +0100

    ex 5.14 blog list tests step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -16,8 +16,6 @@ test('render title and author, don\'t render likes and url at start', () => {
   const titleElement = screen.getByText('Test Title IV', { exact: false })
   const authorElement = screen.getByText('Testius', { exact: false })
 
-  screen.debug(authorElement)
-
   expect(titleElement).toBeDefined()
   expect(authorElement).toBeDefined()
 
@@ -25,4 +23,25 @@ test('render title and author, don\'t render likes and url at start', () => {
   const urlElement = screen.getByText('https://example.com')
   expect(likesElement).not.toBeVisible()
   expect(urlElement).not.toBeVisible()
+})
+
+test('after clicking the button, url and likes are shown', async () => {
+  const blog = {
+    title: 'Test Title IV',
+    author: 'Testius',
+    likes: 5,
+    user: 89139721837,
+    url: 'https://example.com'
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const likesElement = screen.getByText('likes: 5')
+  const urlElement = screen.getByText('https://example.com')
+  expect(likesElement).toBeVisible()
+  expect(urlElement).toBeVisible()
 })
