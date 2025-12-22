e2b02d66cbfd78f3f7197ff5efcf24c04a843957
Pippa van Manen <pippavanmanen@icloud.com>
2025-12-22 20:58:50 +0100

    ex 5.18 blog list end to end testing step 2

--- part5/playwright-bloglist/tests/bloglist.spec.js
+++ part5/playwright-bloglist/tests/bloglist.spec.js
@@ -1,12 +1,33 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        username: 'testificate9',
+        name: 'Test user',
+        password: '1234'
+      }
+    })
     await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
     const locator = page.getByLabel('username')
     await expect(locator).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'testificate9', '1234')
+      await expect(page.getByText('Logged in', { exact: true })).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'testificate9', '12345')
+      await expect(page.getByText('Logged in', { exact: true })).not.toBeVisible()
+    })
   })
 })
