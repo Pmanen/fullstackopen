01fc0e9b82e0ab3d8e3531397d888d0d01221e89
Pippa van Manen <pippavanmanen@icloud.com>
2026-01-11 20:18:35 +0100

    add morgan backend logging

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const morgan = require('morgan')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
@@ -9,6 +10,15 @@ const middleware = require('./utils/middleware')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', function (req) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 mongoose.connect(config.MONGODB_URI, { family: 4 })
 
@@ -21,6 +31,7 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
 
+app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -48,8 +48,13 @@ const userExtractor = async (request, response, next) => {
   next()
 }
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 module.exports = {
   errorHandler,
   tokenExtractor,
   userExtractor,
+  unknownEndpoint,
 }
