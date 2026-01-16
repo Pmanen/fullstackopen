71e20dd2d975b098b5029b42fb1e670e280cc834
Pippa van Manen <pippavanmanen@icloud.com>
2026-01-16 17:16:03 +0100

    ex 9.1-9.3 typescript

---
+++ part9/typescript1/bmiCalculator.ts
@@ -0,0 +1,43 @@
+type BMI = 'Underweight' | 'Normal range' | 'Overweight' | 'Obese';
+
+const calculateBmi = (height: number, weight: number) : BMI => {
+  const result = weight / ((height / 100.0) ** 2);
+  
+  if (result < 18.5) return 'Underweight';
+  else if (result < 25) return 'Normal range';
+  else if (result < 30) return 'Overweight';
+  else if (result < 100) return 'Obese';
+  else throw new Error('Calculation error: BMI is higher than 100')
+}
+
+//console.log(calculateBmi(180, 74))
+//console.log(calculateBmi(179, 64))
+
+const parseArguments = (args: string[]): { height: number, weight: number } => {
+  if (args.length < 4) throw new Error('Not enought arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    if (Number(args[2]) < Number(args[3])) {
+      console.log("Warning: first arg should be height, second should be weight.");
+    }
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not number');
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  const result = calculateBmi(height, weight);
+  console.log(result);
+} catch (error: unknown) {
+  let errorMessage = 'Unknown error';
+  if (error instanceof Error) {
+    errorMessage = `Error: ` + error.message;
+  }
+  console.log(errorMessage);
+}
---
+++ part9/typescript1/exerciseCalculator.ts
@@ -0,0 +1,69 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (hours: number[], target: number): Result => {
+  const periodLength: number = hours.length;
+  const trainingDays: number = hours.filter(hour => hour > 0).length;
+  const average: number = hours.length > 0 
+    ? hours.reduce((acc, hour) => acc + hour, 0) / hours.length 
+    : 0;
+  const success: boolean = average >= target;
+
+  let rating: number;
+  let ratingDescription: string;
+  const averageTargetRatio: number = target > 0 ? average / target : 1;
+  if (averageTargetRatio <= 0.5) {
+    rating = 1;
+    ratingDescription = "low results";
+  }
+  else if (averageTargetRatio <= 0.97) {
+    rating = 2;
+    ratingDescription = "not too bad but could be better";
+  }
+  else {
+    rating = 3;
+    ratingDescription = "good results"
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+const parseCalcArguments = (args: string[]): { hours: number[], target: number } => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.slice(2).some(arg => isNaN(Number(arg)))) throw new Error('Arguments must be valid numbers');
+
+  const target = Number(args[2]);
+  const hours = args.slice(3).map(arg => Number(arg));
+
+  return {
+    hours,
+    target
+  }
+}
+
+try {
+  const { hours, target } = parseCalcArguments(process.argv);
+  const result = calculateExercises(hours, target);
+  console.log(result)
+} catch (error: unknown) {
+  let errorMessage = 'Unknown error'
+  if (error instanceof Error) {
+    errorMessage = `Error: ` + error.message
+  }
+  console.log(errorMessage)
+}
