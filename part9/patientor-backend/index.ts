dcf4f8afbf58c6d07b22605056a502d11772ddf1
Pippa van Manen <pippavanmanen@icloud.com>
2026-01-18 14:52:06 +0100

    ex 9.9 patientor backend step 2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -2,9 +2,12 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+const cors = require('cors')
+app.use(cors())
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
