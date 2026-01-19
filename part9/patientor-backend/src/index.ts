75c0405ceef80b56210feeb2f16b600599862676
Pippa van Manen <pippavanmanen@icloud.com>
2026-01-19 13:57:16 +0100

    ex 9.10 patientor backend step 3

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
 
 const PORT = 3001;
 
-const cors = require('cors')
-app.use(cors())
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
